bde8eedbdfdb5d8467434961ba010f7350212ed9
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-07-17 04:01:19 -0400

    Cleaner Solution

--- simple_multiplication.py
+++ simple_multiplication.py
@@ -1,6 +1,12 @@
 # This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 
+# cleaner solution
+def simple_multiplication(number):
+    return number * 8 if number % 2 == 0 else number * 9
+
+
+# first solution
 def simple_multiplication(number):
     if number % 2 == 0:
         return number * 8
