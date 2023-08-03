9d715ef24760d9ae47694426410fb538d9e150c6
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-08-03 15:49:46 -0400

    Cleaner Code

--- whats_the_real_floor.py
+++ whats_the_real_floor.py
@@ -25,3 +25,8 @@ def get_real_floor(n):
         return n - 1
     elif n > 13:
         return n - 2
+
+
+# Cleaner Code
+def get_real_floor(n):
+    return n if n < 1 else n - 1 if n < 13 else n - 2
