663f27adab403f799514f664771da3af2bfbc78f
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2023-08-27 14:22:49 -0400

    Created reversed_sequence.py

---
+++ reversed_sequence.py
@@ -0,0 +1,12 @@
+# Build a function that returns an array of integers from n to 1 where n>0.
+
+# Example : n=5 --> [5,4,3,2,1]
+
+
+def reverse_seq(n):
+    result = []
+
+    for num in range(1, n + 1):
+        result.append(num)
+
+    return result[::-1]
